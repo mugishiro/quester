ba66e5a2e55659db260f016bb12e49d12ca4a883
mugishiro <force-one0624@hotmail.co.jp>
2019-07-03 10:00:14 +0000

    create post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:twitter]
-  
+   # Include default devise modules. Others available are:
+   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable,
+          :omniauthable, omniauth_providers: [:twitter]
+   has_many :posts
+   
    def self.find_for_oauth(auth)
      user = User.where(uid: auth.uid, provider: auth.provider).first
 
