4c74c340895aae1a32765798f68d569b9137d0d2
mugishiro <nowhere.rev2@gmail.com>
2020-04-08 23:27:51 +0900

    postのstatusコラムのdefault -> true

---
+++ db/migrate/20200408142037_add_default_to_posts.rb
@@ -0,0 +1,8 @@
+class AddDefaultToPosts < ActiveRecord::Migration[5.2]
+  def up
+    change_column_default(:posts, :status, true)
+  end
+  def down
+    change_column_default(:posts, :status, nil)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_044320) do
+ActiveRecord::Schema.define(version: 2020_04_08_142037) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id"
-    t.boolean "status"
+    t.boolean "status", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
