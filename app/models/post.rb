5bad1fb8b9f287fbf45b611ced69c7cfd6a30d29
mugishiro <nowhere.rev2@gmail.com>
2020-04-18 18:05:17 +0900

    ogp for s3

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
 
   validates :content, presence: true, length: { maximum: 1000 }
 
-  def rotate
-    image.variant(rotate: "-180", draw: "text 0,0 'hoge'")
+  def image_url
+    image.attachment.service.send(:object_for, image.key).public_url
   end
 
   def resize
