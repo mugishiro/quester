444c1d0990d5f39b746024e9693796a0e87a3862
mugishiro <nowhere.rev2@gmail.com>
2020-04-22 16:45:01 +0900

    devise routes 設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   get 'toppages/follows'
   get 'toppages/edit'
 
-  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, only: [:omniauth_callbacks],
+             controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_scope :user do
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+    delete 'registration' => 'devise/registrations#destroy', as: :registration 
+  end
   resources :users, only: [:show], param: :nickname do
     resources :posts, only: [:show, :create, :update, :destroy] do
       post :confirm, action: :confirm_new, on: :new
