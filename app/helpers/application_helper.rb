e872db6947f85535eb41a2d1839ec1e4bb07b443
mugishiro <nowhere.rev2@gmail.com>
2020-04-23 22:45:58 +0900

    名前変更, テキスト折り返しなど

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,14 @@
 module ApplicationHelper
   def default_meta_tags
    {
-     site: 'hoge',
-     title: 'hoge',
-     reverse: true,
+     site: 'Quester',
      charset: 'utf-8',
-     description: 'situmon',
-     keywords: 'hoge',
      canonical: request.original_url,
-     separator: '|',
-     icon: [
-       { href: image_url('favicon.ico') },
-       { href: image_url('box.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
-     ],
+     icon: image_url('favicon.png'),
      og: {
-       site_name: 'hoge', # もしくは site_name: :site
-       title: 'hoge', # もしくは title: :title
-       description: 'hoge', # もしくは description: :description
        type: 'website',
        url: request.original_url,
-       image: 'https://tk.ismcdn.jp/mwimgs/e/b/1140/img_eb31afc9c1fb914d68a7c73b657c7ebe183087.jpg',
-       # image: image_url('/assets/box.jpg'),
+       image: image_url('default.png'),
        locale: 'ja_JP',
      },
      twitter: {
