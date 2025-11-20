9bb76d4b008a2102fb26de8dd64629a51a1978a8
u_u <n.catalytic@icloud.com>
2025-11-21 00:42:55 +0900

    Improve avatar fallback and share UI

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def avatar_url(user)
     user&.image_url.presence || "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png"
   end
+
+  def safe_description(user)
+    user&.description.presence || " "
+  end
 end
