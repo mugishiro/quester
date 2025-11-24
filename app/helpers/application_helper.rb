6dffa9689b58bdb48a39e22de48b6aa21a7c8ba9
u_u <n.catalytic@icloud.com>
2025-11-24 18:19:58 +0900

    Unify UI styles and polish meta/imagery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,34 @@
 module ApplicationHelper
   def default_meta_tags
+    title = content_for?(:title) ? yield(:title) : 'Quester - 匿名で質問を集めよう'
+    description = content_for?(:description) ? yield(:description) : '匿名で質問を募り、回答を集めるためのサービスです。'
+    current_url = request.original_url
+    share_image = image_url('default.png')
+    favicon_path = image_url('favicon.png')
+
     {
       site: 'Quester',
+      title: title,
+      description: description,
       charset: 'utf-8',
-      canonical: request.original_url,
-      icon: image_url('favicon.png'),
+      canonical: current_url,
+      icon: [
+        { href: favicon_path },
+        { href: image_url('favicon.ico'), rel: 'shortcut icon', type: 'image/x-icon' },
+      ],
       og: {
         type: 'website',
-        url: request.original_url,
-        image: image_url('default.png'),
+        title: title,
+        description: description,
+        url: current_url,
+        image: share_image,
         locale: 'ja_JP',
       },
       twitter: {
         card: 'summary_large_image',
+        title: title,
+        description: description,
+        image: share_image,
       },
     }
   end
