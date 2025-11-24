0b2232a584ebe8eaf0719065e35e641917ac1ce4
u_u <n.catalytic@icloud.com>
2025-11-24 18:41:36 +0900

    Add 1000-char counters for questions and replies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,32 +17,27 @@
 //= require jquery
 //= require jquery_ujs
 
-$(function (){
-  // 処理（ページが読み込まれた時、フォームに残り何文字入力できるかを数えて表示する）
-  var max = 500;
-  //フォームに入力されている文字数を数える
-  //\nは"改行"に変換して2文字にする。オプションフラグgで文字列の最後まで\nを探し変換する
-  var count = $(".js-text").text().replace(/\n/g, "改行").length;
-  //残りの入力できる文字数を計算
-  var now_count = max - count;
-  //文字数がオーバーしていたら文字色を赤にする
-  if (count > max) {
-    $(".js-text-count").css("color","red");
-  }
-  //残りの入力できる文字数を表示
-  $(".js-text-count").text( 0 + "/" + max);
+$(function () {
+  var MAX_CHARS = 1000;
 
-  $(".js-text").on("keyup", function() {
-    // 処理（キーボードを押した時、フォームに残り何文字入力できるかを数えて表示する）
-    //フォームのvalueの文字数を数える
-    var count = $(this).val().replace(/\n/g, "改行").length;
-    var now_count = max - count;
+  function updateCounter($area) {
+    var count = ($area.val() || "").replace(/\n/g, "改行").length;
+    var $counter = $area.closest(".js-text-wrapper").find(".js-text-count");
+    if (!$counter.length) return;
 
-    if (count > max) {
-      $(".js-text-count").css("color","red");
+    $counter.text(count + "/" + MAX_CHARS);
+    if (count > MAX_CHARS) {
+      $counter.css("color", "red");
     } else {
-      $(".js-text-count").css("color","black");
+      $counter.css("color", "black");
     }
-    $(".js-text-count").text( max - now_count + "/" + max);
+  }
+
+  $(".js-text").each(function () {
+    var $area = $(this);
+    updateCounter($area);
+    $area.on("keyup input change", function () {
+      updateCounter($area);
+    });
   });
 });
