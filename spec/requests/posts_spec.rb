5ad9167db055e1d3d3a2276a76ebb4e6255f1074
u_u <n.catalytic@icloud.com>
2025-11-25 01:09:43 +0900

    Fix post social card image

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,6 +10,16 @@ RSpec.describe "Posts", type: :request do
       get user_post_path user.nickname, test_post.id
       expect(response.status).to eq 200
     end
+
+    it "exposes the post image for social sharing" do
+      test_post.image.attach(io: File.open(Rails.root.join('app/assets/images/default.png')), filename: 'default.png', content_type: 'image/png')
+
+      get user_post_path user.nickname, test_post.id
+
+      image_url = test_post.image_url(host: 'http://www.example.com')
+      expect(response.body).to include(%(property="og:image" content="#{image_url}"))
+      expect(response.body).to include(%(name="twitter:image" content="#{image_url}"))
+    end
   end
 
   describe "POST #confirm_new" do
